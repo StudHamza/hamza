2ee8d9f2025d2eb0bb62c6cdbfa6f60120c16f00
StudHamza <hamza.mohammed.hasan@gmail.com>
2024-07-01 19:20:32 +0300

    first commit

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+
+export default function blog() {
+  return (
+    <Layout>
+    <h1 className="text-3xl font-bold underline">
+      No blogs yet
+    </h1>
+</Layout>
+  )
+}
